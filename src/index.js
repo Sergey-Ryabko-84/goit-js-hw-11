69910477d48ecdf609cd870c996555e8044545ba
Sergey-Ryabko-84 <amigoswow@ukr.net>
2022-10-22 03:31:08 +0300

    Adds gallery markup on submit form

--- src/index.js
+++ src/index.js
@@ -1,4 +1,37 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { fetchGallery } from './js/fetch-gallery';
+import markupGallery from './js/templates/markup-gallery.hbs'
 
 
-fetchGallery('flower').then(console.log)
+const refs = {
+    searchFormEl: document.querySelector('#search-form'),
+    galleryBox: document.querySelector('.gallery'),
+}
+
+refs.searchFormEl.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(e) {
+    e.preventDefault();
+    const searchQuery = e.target.searchQuery.value;
+    const pageNumber = 1;
+    if (!searchQuery.trim()) {
+        Notify.failure('Please, enter a query!', {timeout: 5000, clickToClose: true,});
+        return;
+    }
+    if (searchQuery.length > 100) {
+        Notify.failure('The search query  may not exceed 100 characters! Enter a shorter query.', {timeout: 5000, clickToClose: true,});
+        return;
+    }
+    fetchGallery(searchQuery, pageNumber)
+    .then(data => {
+        galleryMarkup(data);
+    })
+    .catch(console.log)
+}
+
+function galleryMarkup(data) {
+    refs.galleryBox.innerHTML = markupGallery(data);
+    let galleryClick = new SimpleLightbox('.photo-card-link');
+}
--- src/js/fetch-gallery.js
+++ src/js/fetch-gallery.js
@@ -3,27 +3,36 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '30719665-02331968f49df94435bfd7c2d';
-const params = new URLSearchParams({
-    key: API_KEY,
-    q: '',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 40,
-});
 
-export async function fetchGallery (q) {
-    params.q = q;
-    try {
-        const response = await axios.get(`${BASE_URL}?${params}`);        
-        // console.log(response);
-        if (response.data.totalHits > 0) {
-            Notify.success(`Hooray! We found ${response.data.totalHits} images`, {timeout: 5000,});
-            return response.data.hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
-                return {webformatURL, largeImageURL, tags, likes, views, comments, downloads}
-            });
-        } else {            
-            Notify.failure('Sorry, there are no images matching your search query. Please try again.', {timeout: 5000,});
-        }
-    } catch (err) {console.log(err)}
+export async function fetchGallery (searchQuery, pageNumber) {
+    const params = new URLSearchParams({
+        key: API_KEY,
+        q: searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+        page: pageNumber,
+    });
+    const response = await axios.get(`${BASE_URL}?${params}`);        
+    if (response.data.totalHits > 0) {
+        Notify.success(`Hooray! We found ${response.data.totalHits} images`, {timeout: 5000, clickToClose: true,});
+        return response.data.hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
+            return {webformatURL, largeImageURL, tags, likes, views, comments, downloads}
+        });
+    } else {            
+        Notify.failure('Sorry, there are no images matching your search query. Please try again.', {timeout: 5000, clickToClose: true,});
+    }
+    // try {
+    //     const response = await axios.get(`${BASE_URL}?${params}`);        
+    //     // console.log(response);
+    //     if (response.data.totalHits > 0) {
+    //         Notify.success(`Hooray! We found ${response.data.totalHits} images`, {timeout: 5000, clickToClose: true,});
+    //         return response.data.hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
+    //             return {webformatURL, largeImageURL, tags, likes, views, comments, downloads}
+    //         });
+    //     } else {            
+    //         Notify.failure('Sorry, there are no images matching your search query. Please try again.', {timeout: 5000, clickToClose: true,});
+    //     }
+    // } catch (err) {console.log(err)}
 }
