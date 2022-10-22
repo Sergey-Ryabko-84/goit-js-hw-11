e5543396b5dbf30a83c503af784def72bb4ffa13
Sergey-Ryabko-84 <amigoswow@ukr.net>
2022-10-23 02:14:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ refs.loadMoreBtn.addEventListener('click', onClickLoadMoreBtn)
 async function onSubmitForm(e) {
     e.preventDefault();
     refs.loadMoreBtn.hidden = true;
+    refs.galleryBox.innerHTML = '';
     searchQuery = e.target.searchQuery.value;
     pageNumber = 1;
     if (!searchQuery.trim()) {
@@ -29,8 +30,7 @@ async function onSubmitForm(e) {
     if (searchQuery.length > 100) {
         Notify.failure('The search query  may not exceed 100 characters! Enter a shorter query.', {timeout: 5000, clickToClose: true,});
         return;
-    }
-    refs.galleryBox.innerHTML = '';    
+    }      
     galleryMarkup(await fetchGallery(searchQuery, pageNumber));
 }
 
